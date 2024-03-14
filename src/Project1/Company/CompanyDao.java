764b90416d78a489aa2fdc2a41d3efe4a7459824
hwang <hwang@DESKTOP-M7V9G18>
2024-03-14 14:13:55 +0900

    companyservice 작성중2

--- src/Project1/Company/CompanyDao.java
+++ src/Project1/Company/CompanyDao.java
@@ -17,8 +17,9 @@ public class CompanyDao {
 	}
 
 	// 기업 정보 등록
-	public void insertCompany(Company c) {
+	public int insertCompany(Company c) {
 		Connection conn = db.conn();
+		int cnt = 0;
 		String sql = "insert into company values(0,company_seq.nextval,?,?,?,?,?,?)";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
@@ -28,7 +29,7 @@ public class CompanyDao {
 			ps.setString(4, c.getTell());
 			ps.setString(5, c.getEmail());
 			ps.setString(6, c.getAddr());
-			ps.executeUpdate();
+			cnt = ps.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,11 +41,13 @@ public class CompanyDao {
 				e.printStackTrace();
 			}
 		}
+		return cnt;
 	}
 
 	// 기업 번호로 정보 수정 - 기업이름, 기업분야, 기업전화번호, 기업 이메일, 기업주소
-	public void updateCompany(Company c, int cnum) {
+	public int updateCompany(Company c, int cnum) {
 		Connection conn = db.conn();
+		int cnt = 0;
 		String sql = "update company set cname=?, field=?, tell=?, email=?, addr=? where cnum=?";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
@@ -54,7 +57,7 @@ public class CompanyDao {
 			ps.setString(4, c.getEmail());
 			ps.setString(5, c.getAddr());
 			ps.setInt(6, cnum);
-			int cnt = ps.executeUpdate();
+			cnt = ps.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -66,16 +69,18 @@ public class CompanyDao {
 				e.printStackTrace();
 			}
 		}
+		return cnt;
 	}
 
 	// 기업 정보 삭제
-	public void deleteCompany(int cnum) {
+	public int deleteCompany(int cnum) {
 		Connection conn = db.conn();
+		int cnt = 0;
 		String sql = "delete from company where cnum=?";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
 			ps.setInt(1, cnum);
-			ps.executeUpdate();
+			cnt = ps.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -87,16 +92,17 @@ public class CompanyDao {
 				e.printStackTrace();
 			}
 		}
+		return cnt;
 	}
 
 	// 기업 정보 조회 (분야)
-	public ArrayList<Company> selectCompanyByField(Company c) {
+	public ArrayList<Company> selectCompanyByField(String field) {
 		ArrayList<Company> list = new ArrayList<>();
 		Connection conn = db.conn();
 		String sql = "select * from company where field =?";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, c.getField());
+			ps.setString(1, field);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
 				list.add(new Company(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(6),
--- src/Project1/Company/CompanyService.java
+++ src/Project1/Company/CompanyService.java
@@ -8,8 +8,8 @@ public class CompanyService {
 	public CompanyService() {
 		dao = new CompanyDao();
 	}
-	
-	//기업 등록
+
+	// 기업 등록
 	public void addCompany(Scanner sc) {
 		System.out.println("===기업등록===");
 		System.out.print("기업 이름:");
@@ -24,12 +24,58 @@ public class CompanyService {
 		String email = sc.next();
 		System.out.print("기업 주소:");
 		String addr = sc.next();
-		
+		Company p = new Company(0, 0, cname, field, id, tell, email, addr);
+		int cnt = dao.insertCompany(p);
+		if (cnt > 0) {
+			System.out.println("기업 등록 완료");
+		} else {
+			System.out.println("기업 등록 실패");
+		}
+
+	}
+
+	// 기업 수정 기업이름, 기업분야, 기업전화번호, 기업 이메일, 기업주소
+	public void editCompany(Scanner sc) {
+		System.out.println("===기업 수정===");
+		System.out.print("수정할 기업이름:");
+		String cname = sc.next();
+		System.out.print("수정할 기업분야:");
+		String field = sc.next();
+		System.out.print("수정할 기업전화번호:");
+		String tell = sc.next();
+		System.out.print("수정할 기업이메일:");
+		String email = sc.next();
+		System.out.print("수정할 기업주소:");
+		String addr = sc.next();
+		Company p = new Company(0, 0, cname, field, "null", tell, email, addr);
+		int cnum = p.getCnum();
+		int cnt = dao.updateCompany(p, cnum);
+		if (cnt > 0) {
+			System.out.println("기업 수정 완료");
+		} else {
+			System.out.println("기업 수정 실패");
+		}
+	}
+
+	// 기업 삭제
+	public void deleteCompany(Scanner sc) {
+		System.out.println("===기업 삭제===");
+		System.out.print("삭제할 기업번호:");
+		int cnum = sc.nextInt();
+		int cnt = dao.deleteCompany(cnum);
+		if (cnt > 0) {
+			System.out.println("기업 삭제 완료");
+		} else {
+			System.out.println("기업 삭제 실패");
+		}
+	}
+	
+	// 기업 정보 조회 (분야)
+	public void printCompany(Scanner sc){
+		System.out.println("===기엄 정보 조회===");
+		System.out.print("찾을 ")
 		
 	}
-	//기업 수정
-	//기업 삭제
-	//기업 정보 조회 (분야)
-	//기업 정보 전체조회
+	// 기업 정보 전체조회
 
 }
