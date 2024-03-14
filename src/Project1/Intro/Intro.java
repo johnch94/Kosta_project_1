4128e11b5a8936a50812a3b0e8e6a805c7d3654d
Kosta <Kosta@DESKTOP-L0RSBBK>
2024-03-14 11:44:11 +0900

    intro vo, dao 생성 작업 commit

--- src/Project1/Intro/Intro.java
+++ src/Project1/Intro/Intro.java
@@ -1,4 +1,68 @@
-package Project1.Intro;
+package src.Project1.Intro;
 
 public class Intro {
+	private int unum;
+	private String title;
+	private String content;
+	private int id;
+	private String submit;
+
+	public Intro() {
+	}
+
+	public Intro(int unum, String title, String content, int id, String submit) {
+		super();
+		this.unum = unum;
+		this.title = title;
+		this.content = content;
+		this.id = id;
+		this.submit = submit;
+	}
+
+	public int getUnum() {
+		return unum;
+	}
+
+	public void setUnum(int unum) {
+		this.unum = unum;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSubmit() {
+		return submit;
+	}
+
+	public void setSubmit(String submit) {
+		this.submit = submit;
+	}
+
+	@Override
+	public String toString() {
+		return "Intro [unum=" + unum + ", title=" + title + ", content=" + content + ", id=" + id + ", submit="
+				+ submit + "]";
+	}
+
 }
---
+++ src/Project1/Intro/IntroDao.java
@@ -0,0 +1,194 @@
+package src.Project1.Intro;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import src.JDBC.DBConnect;
+
+public class IntroDao {
+	private DBConnect db;
+
+	public IntroDao() {
+		db = DBConnect.getInstance();
+	}
+
+	public void insert(Intro i) {
+		Connection conn = db.conn();
+
+		String sql = "insert into intro values(0,?,?,seql_intro,?)";
+
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, i.getTitle());
+			pstmt.setString(1, i.getContent());
+			pstmt.setString(1, i.getSubmit());
+
+			int cnt = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public void update(Intro i) {
+		Connection conn = db.conn();
+
+		String sql = "update intro set title=?, content=?, submit=?";
+
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, i.getTitle());
+			pstmt.setString(1, i.getContent());
+			pstmt.setString(1, i.getSubmit());
+
+			int cnt = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void delete(int id) {
+		Connection conn = db.conn();
+
+		String sql = "delete from intro where id=?";
+
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+
+			int cnt = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public Intro selectById(int id) {
+		Connection conn = db.conn();
+		String sql = "select * from intro where id=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return new Intro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public ArrayList<Intro> selectByTitle(String title) {
+		Connection conn = db.conn();
+		String sql = "select * from intro where title like ? order by id";
+		ArrayList<Intro> list = new ArrayList<>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "%" + title + "%");
+
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				list.add(new Intro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+
+	public ArrayList<Intro> selectByContent(String content) {
+		Connection conn = db.conn();
+		String sql = "select * from intro where content like ? order by id";
+		ArrayList<Intro> list = new ArrayList<>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "%" + content + "%");
+
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				list.add(new Intro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+
+	public ArrayList<Intro> selectAll() {
+		Connection conn = db.conn();
+		String sql = "select * from intro order by id";
+		ArrayList<Intro> list = new ArrayList<>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				list.add(new Intro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+}
+
